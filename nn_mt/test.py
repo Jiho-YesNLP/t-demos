6287369273d3b57c590ab7bb8a62ea88c8eb0bed
Jiho Noh <jnoh3@kennesaw.edu>
2022-04-05 13:23:53 -0400

    test update

--- nn_mt/test.py
+++ nn_mt/test.py
@@ -13,7 +13,7 @@ import utils
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-saved = torch.load('data/mt.mdl')
+saved = torch.load('data/mt1.mdl')
 model = MachineTranslator(saved['in_lm'], saved['out_lm'], device, 
                           max_length=saved['max_length'])
 model.encoder.load_state_dict(saved['enc.state_dict'])
@@ -23,7 +23,7 @@ def translate(input):
     s = utils.normalizeString(input[:saved['max_length']])
     s = saved['in_lm'].to_tensor(s).to(device)
     translated = model.forward(s)
-    print('translated: {}'.format(''.join(translated[1])))
+    print('translated: {}'.format(' '.join(translated[1])))
 
 with torch.no_grad():
     code.interact(local=dict(locals(), **globals()))
